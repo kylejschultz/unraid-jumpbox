b5e141e11fd36a9691895c3c3486cd1e43fcafea
Kyle Schultz <kyle@kjschultz.com>
2024-09-01 11:25:16 -0700

    add debug to user-auth script for testing

--- ssh-user-auth.sh
+++ ssh-user-auth.sh
@@ -3,8 +3,13 @@
 # Load the settings, which come from Docker environment variables originally.
 . /etc/jump-settings
 
+# Debugging output
+echo "ssh-user-auth.sh executed" >> /var/log/ssh-user-auth.log
+echo "JUMP_USER=$JUMP_USER" >> /var/log/ssh-user-auth.log
+echo "JUMP_PUBLIC_KEY=$JUMP_PUBLIC_KEY" >> /var/log/ssh-user-auth.log
+
 # If the user is the one and only jump user, then return the one and only
-# public key.  If the authentication attempt is using a corresponding private
+# public key. If the authentication attempt is using a corresponding private
 # key, the authentication attempt will succeed.
 if [ "$1" == "$JUMP_USER" ]; then
   echo "$JUMP_PUBLIC_KEY"
